241a1835582b5aed4d2566e378fcc80c6176713e
Zorax12 <livavanaga2003@gmail.com>
2026-02-21 14:52:01 +0200

    updated gameMan with basic stuff

--- Ridzinieka Dzive/Assets/Scripts/GameManager.cs
+++ Ridzinieka Dzive/Assets/Scripts/GameManager.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int money;
+
+    public int hunger = 100;
+    public int energy = 100;
+    public int stress = 0;
+    public int health = 100;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
