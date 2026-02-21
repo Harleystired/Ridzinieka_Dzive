5245b14bd9e1c62317ba1f9d8783d574b8e1ba30
Zorax12 <livavanaga2003@gmail.com>
2026-02-21 15:21:26 +0200

    room done lol

--- Ridzinieka Dzive/Assets/Scripts/GameManager.cs
+++ Ridzinieka Dzive/Assets/Scripts/GameManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int money;
-
     public int hunger = 100;
     public int energy = 100;
     public int stress = 0;
     public int health = 100;
+    
+    public float morning;
+    public float day;
+    public float evening;
+    public float night;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
