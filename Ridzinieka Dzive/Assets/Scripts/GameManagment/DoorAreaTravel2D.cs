e4a2055aac04465e3bbd29b6b76a5e853cdc4f40
Zorax12 <livavanaga2003@gmail.com>
2026-04-01 21:54:43 +0300

    room doors work

---
+++ Ridzinieka Dzive/Assets/Scripts/GameManagment/DoorAreaTravel2D.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public sealed class DoorAreaTravel2D : MonoBehaviour, IClickable2D, IHoverable2D
+{
+    public enum Destination
+    {
+        Computer,
+        Kitchen
+    }
+
+    [Header("Travel")]
+    [SerializeField] private Destination destination = Destination.Kitchen;
+    [SerializeField] private CameraMovement cameraMovement;
+
+    private void Awake()
+    {
+        if (cameraMovement == null)
+            cameraMovement = FindFirstObjectByType<CameraMovement>();
+    }
+
+    public void OnClicked(RaycastHit2D hit)
+    {
+        if (cameraMovement == null) return;
+
+        switch (destination)
+        {
+            case Destination.Kitchen:
+                cameraMovement.Kitchen();
+                break;
+
+            case Destination.Computer:
+                cameraMovement.Computer();
+                break;
+        }
+    }
+
+    // Hover is optional here; keep empty if doors have no special hover behavior.
+    public void OnHoverEnter(RaycastHit2D hit) { }
+    public void OnHoverExit() { }
+}
